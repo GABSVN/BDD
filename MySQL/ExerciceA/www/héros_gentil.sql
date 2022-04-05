af870d0782c0a3d0f730f49c812ae46fc7ff9a46
GABSVN <j.gabriel.fds.1@gmail.com>
2022-04-05 17:11:48 +0200

    4th Commit
    4th Commit

---
+++ MySQL/ExerciceA/www/héros_gentil.sql
@@ -0,0 +1,73 @@
+
+
+SELECT *
+FROM héros_gentils.sql;
+-- WHERE UPPER() LIKE
+
+
+
+
+
+ALTER TABLE "Armes"
+ADD PRIMARY KEY ( Armes ) ;
+INSERT INTO armes (BatteEnMousse, VanneDeLiujen, Microsoft, Feuille, Caillou, Ciseau)
+VALUES(0, 10, 20, 5, 5, 5)
+
+-- ID` tinyint(1) 
+-- HerosID` tinyint(1) 
+-- Nom` varchar(50) 
+-- Type` varchar(20) 
+-- Degat` tinyint(2) 
+
+
+ALTER TABLE "Héros"
+ADD PRIMARY KEY ( Héros ) ;
+INSERT INTO armes (ComarLeJoyeux, KirderfLeCombatif, LiujenLeFlegme)
+VALUES(40, 35, 45)
+
+
+-- ID` tinyint(1)
+-- Nom` varchar(50)
+-- PV` tinyint(2)
+-- Bonus` tinyint(2
+-- Defense` tinyint(2)
+-- ArmeEnMain` tinyint(2)
+-- Statut` varchar(100)
+-- Immunite` varchar(20)
+
+
+ALTER TABLE "Competences"
+ADD PRIMARY KEY ( Competences ) ;
+INSERT INTO armes ()
+VALUES()
+
+
+-- ID` tinyint(1)
+-- Type` ENUM('Statut', 'PV', 'Defense',
+-- 'Bonus', 'ArmeEnMain', 'Immunite'),
+-- Effet` varchar(50)
+
+
+
+
+
+
+
+-- ALTER TABLE nom_table
+-- ADD PRIMARY KEY ( nom_colonne ) ;
+
+
+
+
+
+-- <div class="row align-items-center">
+--     <div class="col">
+--       Armes
+--     </div>
+--     <div class="col">
+--       Héros
+--     </div>
+--     <div class="col">
+--       Competences
+--     </div>
+--   </div>
